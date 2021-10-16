c045fef427aa285c2846dba2c3e3abf89ef2243e
Deepak Chawla <deepakchawla078@gmail.com>
2021-10-16 15:12:23 +0500

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,5 +2,5 @@ import React from 'react'
 import './Header.css';
 
 export default function Header() {
-    return <span onClick={()=> window.scroll(0,0)} className='header'>📺 Dee Movies Hub 🎥  </span>
+    return <span onClick={()=> window.scroll(0,0)} className='header'>🎬 Dee Movies Hub 🎥  </span>
 }
